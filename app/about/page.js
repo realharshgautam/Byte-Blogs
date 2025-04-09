9332d1ca7fde77b0f205566ef1b5093570f6f73b
Harsh Gautam <Macbook@Harshs-MacBook-Air.local>
2025-04-09 23:17:41 +0530

    WIP: Update about page

--- app/about/page.js
+++ app/about/page.js
@@ -33,7 +33,7 @@ export default function About() {
       <section className="py-16 bg-gray-50 dark:bg-gray-800 dark:text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
-            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Harsh &rsquo s Journey as a Coder</h2>
+            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white"> Journey as a Coder</h2>
             <p className="mt-4 text-lg text-gray-600 dark:text-gray-50">
               {"From curious beginner to seasoned developer, here's how Harsh navigated the world of programming."}
             </p>
